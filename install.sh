db8609bb3858788ac8593c463ebb3b917945e349
Matt Bentley <mbentley@mbentley.net>
2022-06-17 12:16:46 -0400

    Prepare for being able to use OpenJDK 17 for 5.4 and above; fixes #210
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- install.sh
+++ install.sh
@@ -2,40 +2,81 @@
 
 set -e
 
+# omada controller dependency and package installer script for versions 4.x and 5.x
+
+# set default variables
 OMADA_DIR="/opt/tplink/EAPController"
 ARCH="${ARCH:-}"
 OMADA_VER="${OMADA_VER:-}"
 OMADA_TAR="${OMADA_TAR:-}"
 OMADA_URL="${OMADA_URL:-}"
-OMADA_MAJOR_VER="$(echo "${OMADA_VER}" | awk -F '.' '{print $1}')"
+OMADA_MAJOR_VER="${OMADA_VER%.*.*}"
+OMADA_MAJOR_MINOR_VER="${OMADA_VER%.*}"
 
 die() { echo -e "$@" 2>&1; exit 1; }
 
+
+echo "**** Selecting packages based on the architecture and version ****"
 # common package dependencies
 PKGS=(
   gosu
   net-tools
-  openjdk-8-jre-headless
   tzdata
   wget
 )
 
+# add specific package for mongodb
 case "${ARCH}" in
-amd64|arm64|"")
-  PKGS+=( mongodb-server-core )
-  ;;
-armv7l)
-  PKGS+=( mongodb )
-  ;;
-*)
-  die "${ARCH}: unsupported ARCH"
-  ;;
+  amd64|arm64|"")
+    PKGS+=( mongodb-server-core )
+    ;;
+  armv7l)
+    PKGS+=( mongodb )
+    ;;
+  *)
+    die "${ARCH}: unsupported ARCH"
+    ;;
 esac
 
+# add specific package for openjdk
+case "${ARCH}" in
+  amd64|arm64|"")
+    # use openjdk-17 for v5.4 and above; all others us openjdk-8
+    case "${OMADA_MAJOR_VER}" in
+      5)
+        # pick specific package based on the major.minor version
+        case "${OMADA_MAJOR_MINOR_VER}" in
+          5.0|5.1|5.3)
+            # 5.0 to 5.3 all use openjdk-8
+            PKGS+=( openjdk-8-jre-headless )
+            ;;
+          *)
+            # starting with 5.4, openjdk-17 is supported
+            PKGS+=( openjdk-17-jre-headless )
+            ;;
+        esac
+        ;;
+      *)
+        # all other versions, use openjdk-8
+        PKGS+=( openjdk-8-jre-headless )
+        ;;
+    esac
+    ;;
+  armv7l)
+    # always use openjdk-8 for armv7l
+    PKGS+=( openjdk-8-jre-headless )
+    ;;
+  *)
+    die "${ARCH}: unsupported ARCH"
+    ;;
+esac
+
+# output variables/selections
 echo "ARCH=${ARCH}"
 echo "OMADA_VER=${OMADA_VER}"
 echo "OMADA_TAR=${OMADA_TAR}"
 echo "OMADA_URL=${OMADA_URL}"
+echo "PKGS=( ${PKGS[*]} )"
 
 echo "**** Install Dependencies ****"
 export DEBIAN_FRONTEND=noninteractive
